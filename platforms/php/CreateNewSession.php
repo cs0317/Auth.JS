dabf74d3268454266f73dca42a097ccb3eb80df2
Phuong Cao <pcao3@illinois.edu>
2016-04-19 01:50:40 -0500

    use fat free framework for PHP

--- platforms/php/CreateNewSession.php
+++ platforms/php/CreateNewSession.php
@@ -1,4 +1,7 @@
 <?php
+// composer autoloader for required packages and dependencies
+require_once('vendor/autoload.php');
+$f3 = \Base::instance();
 
 session_start();
 
@@ -10,23 +13,28 @@ $AUTHJS_FB_APP_SCOPE = "user_about_me,email";
 $format = "https://www.facebook.com/v2.0/dialog/oauth?response_type=code&scope=%s&redirect_uri=http://%s:%d/login/Facebook&client_id=%d";
 $FB_OAUTH_URL = sprintf($format, $AUTHJS_FB_APP_SCOPE, $AUTHJS_LOCAL_SERVER, $AUTHJS_LOCAL_PORT, $AUTHJS_FB_APP_ID);
 
-Flight::set('FB_OAUTH_URL', $FB_OAUTH_URL);
 
-Flight::route('GET /', function(){
+$f3->set('FB_OAUTH_URL', $FB_OAUTH_URL);
+
+
+$f3->route('GET /', function($f3){
     if ($_SERVER['REMOTE_ADDR'] != "127.0.0.1" and $_SERVER['REMOTE_ADDR'] != "::1"){
         // TODO: return 403 error
         echo "local access only";
         return;
     }
-    Flight::render('index.php', array(
-        'UserID' => $_SESSION['UserID'],
-        'FullName' => $_SESSION['FullName'],
-        'email' => $_SESSION['email']
-    ));
+    $f3->mset(
+        array(
+            'UserID' => $_SESSION['UserID'],
+            'FullName' => $_SESSION['FullName'],
+            'email' => $_SESSION['email']
+        ));
+    echo View::instance()->render('views/index.php');
 });
 
-Flight::route('POST /Auth.JS/php/CreateNewSession.php', function(){
-    $UserID = Flight::request()->data->UserID;
+
+$f3->route('POST /Auth.JS/php/CreateNewSession.php', function(){
+    $UserID = $_POST['UserID'];
     if (strlen($UserID) > 0){
         session_destroy();
         $_SESSION['UserID'] = $UserID;
@@ -35,15 +43,18 @@ Flight::route('POST /Auth.JS/php/CreateNewSession.php', function(){
     }
 });
 
-Flight::route('GET /login', function(){
-    Flight::redirect(Flight::get("FB_OAUTH_URL"));
-});
 
-Flight::route('GET /logout', function(){
-    session_destroy();
-    Flight::redirect('/');
-});
+$f3->route('GET /login',
+           function($f3) {
+               $f3->reroute($f3->get("FB_OAUTH_URL"));
+           }
+);
 
-Flight::start();
+$f3->route('GET /logout',
+           function($f3) {
+               session_destroy();
+               $f3->reroute("/");
+           }
+);
 
-?>
+$f3->run();
